9c4e234342e79619f3751fe256ef500d1d123c3c
zhoutianze-i <zhoutianze-i@01.ai>
2024-10-29 06:38:21 +0000

    feat: python lsp, theme

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -170,6 +170,8 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
+        basedpyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
